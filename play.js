ed6f4d0313506418f984f0bede611f2ee3e729d9
Shoumik-Chowdhury <shoumik.atique@gmail.com>
2021-11-03 18:37:33 +0000

    added name

--- play.js
+++ play.js
@@ -1,6 +1,12 @@
 const {connect} = require("./client");
 
 const server = connect();
+
+server.on('connect', () => { // this is how we receive
+  console.log(`Connected to server.`);
+  server.write("Name: SAC");
+});
+
 server.on('data', function(message){ // this is how we receive
   console.log(`Server says: ${message}`);
   });
